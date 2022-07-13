c0620468d7542e7d0be1c204e877cfb7a531bebe
Miquel Angel <hulkrojo2@gmail.com>
2022-07-13 13:52:49 +0200

    added command to delete ranks

--- src/main/java/es/hulk/core/rank/commands/impl/RankDeleteCommand.java
+++ src/main/java/es/hulk/core/rank/commands/impl/RankDeleteCommand.java
@@ -1,4 +1,34 @@
 package es.hulk.core.rank.commands.impl;
 
-public class RankDeleteCommand {
+import es.hulk.core.Core;
+import es.hulk.core.rank.Rank;
+import es.hulk.core.rank.RankManager;
+import es.hulk.core.utils.command.BaseCommand;
+import es.hulk.core.utils.command.CommandArgs;
+import org.bukkit.entity.Player;
+
+public class RankDeleteCommand extends BaseCommand {
+
+    @Command(name = "rank.delete", permission = "rank.delete")
+    @Override
+    public void onCommand(CommandArgs command) {
+        Player player = command.getPlayer();
+        String[] args = command.getArgs();
+
+        if (args.length == 0) {
+            player.sendMessage("/rank delete <rank>");
+            return;
+        }
+
+        RankManager rankManager = Core.getInstance().getRankManager();
+        Rank rank = rankManager.getRank(args[0]);
+
+        if (rank == null) {
+            player.sendMessage("Rank not found");
+            return;
+        }
+
+        Rank.getRanks().remove(rank);
+        player.sendMessage("Rank deleted");
+    }
 }
