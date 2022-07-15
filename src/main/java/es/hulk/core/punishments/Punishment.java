ff885a9384e49ee4772354f033ebc5e9183e2ca2
Miquel Angel <hulkrojo2@gmail.com>
2022-07-15 19:51:33 +0200

    code cleanup

--- src/main/java/es/hulk/core/punishments/Punishment.java
+++ src/main/java/es/hulk/core/punishments/Punishment.java
@@ -1,11 +1,8 @@
 package es.hulk.core.punishments;
 
-import es.hulk.core.utils.chat.ChatUtil;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
@@ -22,23 +19,6 @@ public class Punishment {
 
     public boolean isPermanent() {
         return this.duration.equalsIgnoreCase("Permanent") || this.expiresOn.equalsIgnoreCase("Never");
-    }
-
-    public void punish() {
-        Player player = Bukkit.getPlayer(this.punishedUUID);
-        if(player == null) return;
-
-        switch(this.type) {
-            case KICK:
-                player.kickPlayer(ChatUtil.translate(this.madeOn + "\n\n&fReason&7: &b" + this.reason));
-                break;
-            case WARN:
-                player.sendMessage(ChatUtil.translate(this.reason + "."));
-                break;
-            case BAN:
-                player.kickPlayer(ChatUtil.translate(this.madeOn + "\n\n&fReason&7: &b" + this.reason + "\n&fExpires In&7: &b" + this.expiresOn));
-                break;
-        }
     }
 
 }
